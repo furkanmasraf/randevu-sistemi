4772990b5e4cc87b7e6cd89d70259081ab7397ce
Furkan Masraf <masraffurkan@gmail.com>
2026-06-24 13:54:17 +0300

    React ön yüzünden gelen isteklerin engellenmemesi için ShopController ve AuthController sınıfının üzerine `@CrossOrigin` anotasyonu eklendi..

--- src/main/java/com/randevukuafor/randevu_sistemi/controller/ShopController.java
+++ src/main/java/com/randevukuafor/randevu_sistemi/controller/ShopController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/shops")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ShopController {
 
     private final ShopService shopService;
