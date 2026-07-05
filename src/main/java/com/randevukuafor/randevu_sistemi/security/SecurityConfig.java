12196bcbf638c955b7ddd8e45f5f094ea643957d
Furkan Masraf <masraffurkan@gmail.com>
2026-07-06 02:52:27 +0300

    Vercel linki eklendi..

--- src/main/java/com/randevukuafor/randevu_sistemi/security/SecurityConfig.java
+++ src/main/java/com/randevukuafor/randevu_sistemi/security/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174, https://randevu-web-six.vercel.app"));
         // 'PATCH' metodu eklendi..
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With"));
